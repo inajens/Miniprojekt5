ce50662d2114a09c4c26e4e046d35fa3dc8f6df5
Ina Jensen <ina.jensen@hotmail.com>
2017-08-23 13:41:14 +0200

    Potentially releasable product

--- Miniprojekt5/target/classes/templates/javascript.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById('helpingnow');
-    if (x.style.display === 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}
